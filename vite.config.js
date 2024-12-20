d95a4492a7987c8ecb47f769b862cba373becf79
akhwan16 <faridakhwan57@gmail.com>
2024-12-20 21:49:04 +0700

    sdsds

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ export default {
   base: '/game-pembangunan-kota/',
 
   // Set the project root directory (relative to the config file)
-  root: '../src',
+  root: 'src/index.html',
 
   // Set the directory to serve static files from (relative to the root)
-  publicDir: '../public',
+  publicDir: 'src/public',
   
   // Set the build output directory
   build: {
-    outDir: '../dist'
+    outDir: './dist'
   }
 }
